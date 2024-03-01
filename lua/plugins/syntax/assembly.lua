621e689d86df9d7c705f6cc526f756b9290bb96c
Pedro <portasemasaneta@gmail.com>
2024-03-01 02:27:19 -0300

    assembly lsp fix

--- lua/plugins/syntax/assembly.lua
+++ lua/plugins/syntax/assembly.lua
@@ -1,7 +1,12 @@
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-require'lspconfig'.asm_lsp.setup {
+local lspconfig = require('lspconfig')
+lspconfig.asm_lsp.setup {
     capabilities = capabilities,
+    cmd ={
+        "asm-lsp"
+    },
+    root_dir = function(fname)
+        return vim.fn.fnamemodify(fname, ':p:h') -- Set the root directory to the directory of the current file
+    end,
 }
-
 
